fe3dbdddde235545d96e5a0e0fc8cab8cf09a099
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-01-18 13:59:44 +0900

    Use latest if ensure => present

--- manifests/init.pp
+++ manifests/init.pp
@@ -39,9 +39,10 @@ class embulk (
 
   ensure_packages('java-1.7.0-openjdk')
 
-  $url = $version ? {
-    latest => 'http://dl.embulk.org/embulk-latest.jar',
-    default  => "https://dl.bintray.com/embulk/maven/embulk-${version}.jar",
+  $url = $ensure ? {
+    latest  => 'http://dl.embulk.org/embulk-latest.jar',
+    present => 'http://dl.embulk.org/embulk-latest.jar',
+    default => "https://dl.bintray.com/embulk/maven/embulk-${ensure}.jar",
   }
 
   $embulk_dir = $user ? {
