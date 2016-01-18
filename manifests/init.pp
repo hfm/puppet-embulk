d1d8cf64505d0025bf9884607ab6db649c549cbe
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-01-18 13:49:35 +0900

    s/created_path/embulk_dir/g
    For easy to understand

--- manifests/init.pp
+++ manifests/init.pp
@@ -44,15 +44,15 @@ class embulk (
     default  => "https://dl.bintray.com/embulk/maven/embulk-${version}.jar",
   }
 
-  $created_path = $user ? {
+  $embulk_dir = $user ? {
     'root'  => '/root/.embulk',
     default => "/home/${user}/.embulk",
   }
 
   file {
     [
-      $created_path,
-      "${created_path}/bin",
+      $embulk_dir,
+      "${embulk_dir}/bin",
     ]:
       ensure => 'directory',
       owner  => $user,
@@ -60,11 +60,11 @@ class embulk (
 
   include ::wget
   ::wget::fetch { $url:
-    destination => "${created_path}/bin/embulk",
+    destination => "${embulk_dir}/bin/embulk",
     user        => $user,
     verbose     => false,
   } ~>
-  file { "${created_path}/bin/embulk":
+  file { "${embulk_dir}/bin/embulk":
     ensure => present,
     owner  => $user,
     mode   => '0755',
