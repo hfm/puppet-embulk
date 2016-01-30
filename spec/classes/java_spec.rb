8e739f441832c2e21e929827c96702270984caa0
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-01-30 21:41:53 +0900

    Fix platform

--- spec/classes/java_spec.rb
+++ spec/classes/java_spec.rb
@@ -7,15 +7,7 @@ describe 'embulk::java' do
 
   context 'debian' do
     let(:facts) do
-      { :operatingsystem => 'Debian' }
-    end
-
-    it { should contain_package('openjdk-7-jre-headless') }
-  end
-
-  context 'ubuntu' do
-    let(:facts) do
-      { :operatingsystem => 'Ubuntu' }
+      { :osfamily => 'Debian' }
     end
 
     it { should contain_package('openjdk-7-jre-headless') }
