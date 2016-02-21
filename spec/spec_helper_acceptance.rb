9cf201629c7ed32bf0b543eed179aff63e65fad2
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-02-22 03:05:34 +0900

    Add acceptance nodesets

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,21 +1,21 @@
 require 'beaker-rspec'
 
 # Install Puppet agent on all hosts
-install_puppet_on(hosts)
-# install_puppet_agent_on(hosts, {})
-
-# Install module to all hosts
-module_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-install_dev_puppet_module(:source => module_root)
-
-# Install dependencies
-hosts.each do |host|
-  apply_manifest_on(host, 'package { "tar": }')
-
-  on(host, puppet('module', 'install', 'puppetlabs-stdlib'))
-  on(host, puppet('module', 'install', 'maestrodev-wget'))
-end
+install_puppet_agent_on(hosts, {})
 
 RSpec.configure do |c|
   c.formatter = :documentation
+  module_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+
+  c.before :suite do
+    # Install module to all hosts
+    install_dev_puppet_module(:source => module_root)
+
+    # Install dependencies
+    hosts.each do |host|
+      apply_manifest_on(host, 'package { "tar": }')
+      on(host, puppet('module', 'install', 'puppetlabs-stdlib'))
+      on(host, puppet('module', 'install', 'maestrodev-wget'))
+    end
+  end
 end
